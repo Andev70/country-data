da9c291a0b811f93ca6e265f60ed11d7d35a35fe
anupal nath <anupalnath70@gmail.com>
2022-12-28 15:38:39 +0530

    created new country app

--- main.js
+++ main.js
@@ -1,36 +1,32 @@
 import "./style.css";
 import "./src/sass/test.scss";
 
-const flagWrap = document.querySelector(".box");
+const parentFlag = document.querySelector(".parent");
+const main = document.querySelector(".box");
 
-const getFlag = async function (country) {
+const getFlag = async function () {
   try {
-    const response = await fetch(
-      `https://restcountries.com/v3.1/name/${country}`
-    );
+    const response = await fetch(`https://restcountries.com/v3.1/all `);
     const data = await response.json();
-    return data[0];
+    return data;
   } catch (error) {
     console.log(`error: ${error}`);
-const failLoader = function () {
-      flagWrap.innerHTML = `<h1 class="er-mg text-gray-400">CONNECTION ERROR</h1>`;
-    };
-    setTimeout(failLoader, 2000);
   }
 };
 
-getFlag("singapore").then((countryData) => {
-  if (countryData !== undefined) {
-    flagWrap.innerHTML = ` <img src="${countryData.flags.png}" class="img">`;
-  } else if (countryData === undefined) {
-    const loader = function () {
-      flagWrap.innerHTML = `<h1 class="er-mg text-gray-400">SOMETHING WENT WORNG</h1>`;
-    };
-    setTimeout(loader, 2000);
-  } else if (countryData.message !== undefined) {
-    const errorLoader = function () {
-      flagWrap.innerHTML = `${countryData.message}`.toUpperCase();
-    };
-    setTimeout(errorLoader, 2000);
-  }
+getFlag().then((countries) => {
+  countries.forEach((country) => {
+    parentFlag.innerHTML += `
+          
+ <div class="flags items-center justify-between">
+              <img src="${country.flags.png}" class="img" />
+              <a class="info-link" href="#">
+                <h1 class="country-name">${country.name.common}</h1>
+              </a>
+            </div>
+          
+           
+    `;
+  });
+  
 });
